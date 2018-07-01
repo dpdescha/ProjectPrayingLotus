28fe33f44151d591b28e55ca490a84149d54b8f6
David Deschamps <dpdescha@gmail.com>
2018-07-01 12:30:37 -0400

    first commit

---
+++ Source/Grenades/GrenadesGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GrenadesGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGrenadesGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGrenadesGameMode();
+};
+
+
+
